9258ce8f3322cb9c73555c5c386194d4415847fb
GitHub Action <action@github.com>
2022-06-12 18:18:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/unthrottled/jetbrainsdemotheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.unthrottled.jetbrainsdemotheme.services
+
+import com.github.unthrottled.jetbrainsdemotheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
